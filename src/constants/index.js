69b935780dcc89d583460fad69d134cd81ca8720
VEEPforge <epvillanueva1@up.edu.ph>
2023-07-14 11:39:00 +0800

    Upload/Install frontend dependencies

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+/*******************************************************************************
+Organize all the static text/elements like logo, icons, text, titles, links, etc.
+*******************************************************************************/
+
+// import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+    current: true,
+  },
+  {
+    id: "taxonomy",
+    title: "Taxonomy",
+    current: false,
+  },
+  {
+    id: "isolation-source",
+    title: "Isolation sources",
+    current: false,
+  },
+  {
+    id: "about",
+    title: "About",
+    current: false,
+  },
+];
