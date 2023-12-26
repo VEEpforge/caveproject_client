960167bf8fc82da1d6b09494dad39e4c7fa15c1d
VEEPforge <epvillanueva1@up.edu.ph>
2023-12-26 21:48:10 +0800

    Added constant files

---
+++ src/constants/caves.js
@@ -0,0 +1,92 @@
+export const caves = [
+  {
+		'name': 'Cavinti Underground River and Cave Complex',
+		'province': 'Laguna',
+		'coordinates': [14.306271243126654, 121.70326780910446]
+	},
+	{
+		'name': 'Cabag Cave',
+		'province': 'Laguna',
+		'coordinates': [14.239726233256526, 120.95207761298605]
+	},
+	{
+		'name': 'Sangab Cave',
+		'province': 'Laguna',
+		'coordinates': [14.657387688633309, 121.33163536482438]
+	},
+	{
+		'name': 'Calinawan Cave',
+		'province': 'Laguna',
+		'coordinates': [14.557720155389793, 121.31446922676803]
+	},
+	{
+		'name': 'Tagong Bato Small Cave',
+		'province': 'Batangas',
+		'coordinates': [13.620724247390939, 121.29924055471592]
+	},
+	{
+		'name': 'Barracuda Cave',
+		'province': 'Batangas',
+		'coordinates': [14.272416980404188, 120.56123402699482]
+	},
+	{
+		'name': 'Kinabuhayan Cave',
+		'province': 'Quezon',
+		'coordinates': [14.091333255172561, 121.43388201173782]
+	},
+	{
+		'name': 'Malaog Cave',
+		'province': 'Quezon',
+		'coordinates': [14.205853609712022, 121.60966326543495]
+	},
+	{
+		'name': 'Sungawan Cave',
+		'province': 'Quezon',
+		'coordinates': [14.051370747775632, 121.65360857885923]
+	},
+	{
+		'name': 'Sibatiya Cave',
+		'province': 'Quezon',
+		'coordinates': [13.9660941020914, 121.79093768331013]
+	},
+	{
+		'name': 'Malusak Cave',
+		'province': 'Quezon',
+		'coordinates': [13.971380156649296, 121.97121006777704]
+	},
+	{
+		'name': 'Bosque Cave',
+		'province': 'Quezon',
+		'coordinates': [13.8815390087003, 122.10293288011098]
+	},
+	{
+		'name': 'Anilon Cave',
+		'province': 'Quezon',
+		'coordinates': [14.952548366400482, 122.02617915947653]
+	},
+	{
+		'name': 'Calinawan Cave',
+		'province': 'Rizal',
+		'coordinates': [14.552459933104721, 121.3579269174289]
+	},
+	{
+		'name': 'Pupot Cave',
+		'province': 'Rizal',
+		'coordinates': [14.551795315579275, 121.32359464131619]
+	},
+	{
+		'name': 'Sangab Caves',
+		'province': 'Rizal',
+		'coordinates': [14.629541963372976, 121.33870084280578]
+	},
+	{
+		'name': 'Mystical Cave',
+		'province': 'Rizal',
+		'coordinates': [14.603629466195992, 121.20755154805515]
+	},
+	{
+		'name': 'Pamitinan Cave',
+		'province': 'Rizal',
+		'coordinates': [14.72984094499473, 121.18901211895428]
+	},
+]
---
+++ src/pages/isolation/index.js
@@ -0,0 +1,9 @@
+import IsolationMap from './IsolationMap';
+import IsolationTable from './IsolationTable';
+import Krona from './Krona'
+
+export {
+  IsolationMap,
+  IsolationTable,
+  Krona,
+}
